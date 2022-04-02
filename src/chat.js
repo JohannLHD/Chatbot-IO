5246817277033c9e2f153e63027146d3fed95d8f
Johann LEHUEDE <johann@MacBook-Pro-de-Johann.local>
2022-04-02 15:48:27 +0200

    Add new content

--- src/chat.js
+++ src/chat.js
@@ -18,11 +18,6 @@ const Chat = class {
       <div id="contacts" class="row mt-4">
         <div class="col-12">
           ${this.renderContact()}
-          ${this.renderContact()}
-          ${this.renderContact()}
-          ${this.renderContact()}
-          ${this.renderContact()}
-          ${this.renderContact()}
         </div> 
       </div>
     `;
@@ -38,7 +33,7 @@ const Chat = class {
         </div>  
         <div class="col-8 pt-1">
           <span class="h5">Deadpool</span>
-          <p class="text-muted fs-6">FUCK you !</p>
+          <p class="text-muted fs-6">Connected</p>
         </div>
       </div>
     `;
@@ -47,26 +42,8 @@ const Chat = class {
   renderMessages() {
     return `
       <div id="messages" class="row">
-        <div class="col-12">
-          ${this.renderMessageSent()}
+        <div class="col-12" id = "box-messages">
           ${this.renderMessageReceived()}
-        </div>
-      </div>
-    `;
-  }
-
-  renderMessageSent() {
-    return `
-      <div class="messages--message-send row">
-        <div class="col-6"></div>
-        <div class="col-6">
-          <div class="card">
-            <h5 class="card-header">Johann</h5>
-            <div class="card-body">
-              <h5 class="card-title">22/01/22 - 11:33</h5>
-              <p class="card-text">Hello how are you ?</p>
-            </div>
-          </div>
         </div>
       </div>
     `;
@@ -74,10 +51,10 @@ const Chat = class {
 
   renderMessageReceived() {
     return `
-      <div class="messages--message--received row">
+      <div class="messages--message--received row mt-2">
         <div class="col-6">
           <div class="card">
-            <h5 class="card-header">Johann</h5>
+            <h5 class="card-header">Deadpool</h5>
             <div class="card-body">
               <h5 class="card-title">22/01/22 - 11:33</h5>
               <p class="card-text">Hello how are you ?</p>
@@ -89,18 +66,73 @@ const Chat = class {
     `;
   }
 
+  renderMessageSent(text) {
+    return `
+      <div class="messages--message-send row mt-2">
+        <div class="col-6"></div>
+        <div class="col-6">
+          <div class="card">
+            <h5 class="card-header">Johann</h5>
+            <div class="card-body">
+              <p class="card-title">${this.renderCurrentTime()}</p>
+              <p class="card-text">${text}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  renderCurrentTime() {
+    const today = new Date();
+    return today.toLocaleTimeString();
+  }
+
+  renderMessageSentTyping() {
+    const content = document.getElementById('exampleFormControlInput1');
+    const btnSend = document.getElementById('send-button');
+    const messages = document.getElementById('box-messages');
+    btnSend.addEventListener('click', () => {
+      const message = document.createElement('div');
+      message.innerHTML = this.renderMessageSent(content.value);
+      messages.append(message);
+      content.value = '';
+    });
+    window.addEventListener('keyup', (event) => {
+      if (event.keyCode === 13) {
+        const message = document.createElement('div');
+        message.innerHTML = this.renderMessageSent(content.value);
+        messages.append(message);
+        content.value = '';
+      }
+    });
+  }
+
+  renderTyping() {
+    return `
+    <div id="typing" class="row align-items-center  mt-4">
+      <div class ="col-10">
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="type message">
+      </div>
+      <div class ="col-2">
+        <button type="button" class="btn btn-primary px-4" id="send-button">Send</button>
+      </div>
+    </div>
+    `;
+  }
+
   render() {
     return `
       ${this.renderHeader()}
-      <div class="container-fluid">
-        <div class="row">
+      <div class="container-fluid mt-4 h-100" id= "chatbox-container">
+        <div class="row h-100">
           <div class="col-2">
             ${this.renderContacts()}
           </div>
           <div class="col-10">
-            <div class="row>
+            <div class="row h-100">
               ${this.renderMessages()}
-              <div id="typing" class="row"></div>
+              ${this.renderTyping()}
             </div>
           </div>
         </div>
@@ -110,6 +142,8 @@ const Chat = class {
 
   run() {
     this.el.innerHTML = this.render();
+
+    this.renderMessageSentTyping();
   }
 };
 
